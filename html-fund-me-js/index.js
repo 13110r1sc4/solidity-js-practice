4c13236d6b14eaa6d92a66cc53e1ff7c383f3181
13110r1sc4 <147632303+13110r1sc4@users.noreply.github.com>
2025-01-15 20:03:15 +0100

    front end development

--- html-fund-me-js/index.js
+++ html-fund-me-js/index.js
@@ -1,15 +1,23 @@
 // in front end cannot use require
 // better use import
+import { ethers } from "./ethers-6.7.0.esm.min.js"
+import { abi } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+// console.log(ethers)
 
 async function connect() {
     // can write here js
     if (typeof window.ethereum !== "undefined") {
         await window.ethereum.request({ method: "eth_requestAccounts" })
-        document.getElementById("connectButton").innerHTML = "Connected!"
+        connectButton.innerHTML = "Connected!"
     } else {
         console.log("No metamask")
-        document.getElementById("connectButton").innerHTML =
-            "Please install meatmask"
+        connectButton.innerHTML = "Please install meatmask"
     }
 }
 
@@ -17,6 +25,13 @@ async function connect() {
 async function fund(ethAmount) {
     console.log(`Funding with ${ethAmount}...`)
     if (typeof window.ethereum !== "undefined") {
+        // provider for conenction to bc
+        // singer / wallet / someone with some gas
+        //
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = "" // ?
+        console.log(signer) // signer is the account connected (public address)
     }
 }
 
