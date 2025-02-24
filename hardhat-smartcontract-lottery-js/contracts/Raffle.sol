1f8ba0c4385dbe51ad3bca0436914d7fa9030a62
13110r1sc4 <147632303+13110r1sc4@users.noreply.github.com>
2025-02-24 18:58:38 +0100

    Chainlink keepers

--- hardhat-smartcontract-lottery-js/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery-js/contracts/Raffle.sol
@@ -10,10 +10,18 @@ pragma solidity ^0.8.26;
 
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 
 error Raffle__NotEnoughETHEntered();
+error Raffle__TransferFailed();
+error Raffle__NotOpen();
 
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    /* Type declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
     /* State Variables */
     uint256 private i_entranceFee;
     address payable[] private s_players;
@@ -24,9 +32,14 @@ contract Raffle is VRFConsumerBaseV2 {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
+    // lottery variables
+    address private s_recentWinner;
+    RaffleState private s_raffleState; // to pending, open, close, calculating
+
     // Events
     event RaffleEnter(address indexed player);
     event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         address vrfCoordinatorV2,
@@ -40,10 +53,14 @@ contract Raffle is VRFConsumerBaseV2 {
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
     }
     function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
         }
         s_players.push(payable(msg.sender));
         // events: emit an event when changing a dynamic array or mapping
@@ -51,10 +68,22 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RaffleEnter(msg.sender);
     }
 
+    /**
+     * @dev this is the function that the chainlink keeper nodes call
+     * they look for te upKeepNeeded to return true.
+     * The following should be true in order to return true:
+     * 1. our time inetreval should have passed
+     * 2. the lottery should have at least one player, and have some ETH
+     * 3. our subscription is funded with LINK
+     * 4. the lottery should be in an "open" state
+     */
+    function checkUpKeep(bytes calldata /* checkData */) external override {}
+
     function requestRandomWinner() external {
         // request the random number
         // do something wit it
         // 2 trans process
+        s_raffleState = RaffleState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
@@ -65,9 +94,19 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RequestedRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+    function fulfillRandomWords(
+        uint256 /*requestId*/, // dont need this var for the app but the function requires this arg
+        uint256[] memory randomWords
+    ) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
     }
 
     function getEntranceFee() public view returns (uint256) {
@@ -76,5 +115,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
